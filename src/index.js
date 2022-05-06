2ea9c656db353950fcfdc779c0e59d2057e1f41d
Blade Barringer <blade@crookedneighbor.com>
2022-05-06 10:41:08 -0500

    fix: remove duplicate entries from SLD double faced oracle cards
    closes #23

--- src/index.js
+++ src/index.js
@@ -110,9 +110,25 @@ btn.addEventListener("click", () => {
           ") prefer:usd-low usd>0";
         batches.push(
           scryfall.search(query).then((res) => {
+            // the SLD full deck has a peculiar quirk, where some magic cards have the same oracle id for 2 faces
+            // Scryfall surfaces those cards as unique cards as far as the search goes, so we need to filter them
+            // out when presenting to the end user
+            // https://github.com/crookedneighbor/mana-tithe/issues/23
+            const ids = {};
+            const payload = res.filter((entry) => {
+              const oracleId = entry.card_faces[0].oracle_id;
+
+              if (!ids[oracleId]) {
+                ids[oracleId] = true;
+                return true;
+              }
+              return false;
+            });
+
+            console.log(payload);
             progress.value += 1;
 
-            return res;
+            return payload;
           })
         );
       }
