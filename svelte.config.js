d6859938f9d7069d66e2f29d492f788e85c6c090
Blade Barringer <blade@crookedneighbor.com>
2023-12-26 21:34:53 -0600

    fix: correct path of base path

--- svelte.config.js
+++ svelte.config.js
@@ -13,10 +13,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: 'index.html'
-		})
-	},
-	paths: {
-		base: process.env.NODE_ENV === 'production' ? '/mana-tithe' : ''
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/mana-tithe' : ''
+		}
 	}
 };
 
