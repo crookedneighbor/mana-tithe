5afed357f33ab2f5f01de136ace457f8f4b2452f
Piotr <103199955+ccandour@users.noreply.github.com>
2024-09-27 18:21:40 +0200

    fix: handle double faced tokens properly

--- src/lib/convert-scryfall-results-to-deck-results.ts
+++ src/lib/convert-scryfall-results-to-deck-results.ts
@@ -2,51 +2,63 @@ import type scryfall from 'scryfall-client';
 import { cardNameToDeckKey, type Deck } from './convert-raw-string-to-deck';
 
 export type CardRow = {
-	qty: number;
-	name: string;
-	price: number;
-	image: string;
-	scryfallLink: string;
-	tcgPlayerLink: string;
+    qty: number;
+    name: string;
+    price: number;
+    image: string;
+    scryfallLink: string;
+    tcgPlayerLink: string;
 };
 
 export type DeckResultOptions = {
-	ignorePriceOfBasicLands: boolean;
+    ignorePriceOfBasicLands: boolean;
 };
 
 export default function convertScryfallResultsToDeckResults(
-	scryfallPayload: Awaited<ReturnType<typeof scryfall.search>>,
-	deck: Deck,
-	options: DeckResultOptions
+    scryfallPayload: Awaited<ReturnType<typeof scryfall.search>>,
+    deck: Deck,
+    options: DeckResultOptions
 ): CardRow[] {
-	return scryfallPayload.map((card) => {
-		const cardKey = cardNameToDeckKey(card.name);
-		const cardInDecklist = deck[cardKey];
+    return scryfallPayload.map((card) => {
+        const cardKey = cardNameToDeckKey(card.name);
+        const cardInDecklist = deck[cardKey];
 
-		let price = Number(card.getPrice());
-		let tcgPlayerLink = card.purchase_uris.tcgplayer;
+		// Handle the case where the card is not in the decklist
+        if (!cardInDecklist) {
+            return {
+                qty: 0,
+                name: card.name,
+                price: 0,
+                image: card.getImage(),
+                scryfallLink: card.scryfall_uri,
+                tcgPlayerLink: ''
+            };
+        }
 
-		switch (cardKey) {
-			case 'island':
-			case 'forest':
-			case 'mountain':
-			case 'swamp':
-			case 'plains':
-				if (options.ignorePriceOfBasicLands) {
-					price = 0;
-					tcgPlayerLink = '';
-				}
-				break;
-			default:
-		}
+        let price = Number(card.getPrice());
+        let tcgPlayerLink = card.purchase_uris.tcgplayer;
 
-		return {
-			qty: cardInDecklist.qty,
-			name: card.name,
-			price,
-			image: card.getImage(),
-			scryfallLink: card.scryfall_uri,
-			tcgPlayerLink
-		};
-	});
+        switch (cardKey) {
+            case 'island':
+            case 'forest':
+            case 'mountain':
+            case 'swamp':
+            case 'plains':
+                if (options.ignorePriceOfBasicLands) {
+                    price = 0;
+                    tcgPlayerLink = '';
+                }
+                break;
+            default:
+        }
+
+        return {
+            qty: cardInDecklist.qty,
+            name: card.name,
+            price,
+            image: card.getImage(),
+            scryfallLink: card.scryfall_uri,
+            tcgPlayerLink
+        };
+    });
 }
--- src/lib/convert-to-scryfall-oracle-ids.ts
+++ src/lib/convert-to-scryfall-oracle-ids.ts
@@ -6,6 +6,19 @@ export default async function convertToScryfallOracleIds(deck: Deck) {
 		return { name: deck[key].name };
 	});
 	const collection = await scryfall.getCollection(deckAsNames);
+
+	// Check for name mismatches and rerun the search if needed
+	for (let i = 0; i < collection.length; i++) {
+		let card = collection[i];
+		if (card.name !== deckAsNames[i].name) {
+			let search: any = await scryfall.get(
+				"cards/search", 
+				{ q: `!"${deckAsNames[i].name}" -is:split -is:flip -is:transform -is:meld -is:leveler -is:dfc -is:mdfc` }
+			);
+			collection[i] = search.data[0];
+		}
+	}
+
 	const oracleIds = collection.map((c) => c.oracle_id);
 
 	return {
